b0312e17522c4100c024fba8e62c05ea2693af71
spectro-felpe <fellipe2012rafa@gmail.com>
2026-09-16 21:11:01 -0300

    Create package-info.js for advancement predicates
    Add package-info.js to define null handling for predicates

---
+++ net/minecraft/advancements/predicates/package-info.js
@@ -0,0 +1,7 @@
+/**
+ * Pacote: net.minecraft.advancements.predicates
+ * Marcado como NullMarked para sinalizar que os componentes
+ * deste pacote não esperam receber parâmetros nulos por padrão.
+ */
+
+window.AdvancementPredicates = window.AdvancementPredicates || {};
