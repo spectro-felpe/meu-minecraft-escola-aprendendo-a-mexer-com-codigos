3fd461d4cc6f860549d6a61866e5cdb3c9e45db8
spectro-felpe <fellipe2012rafa@gmail.com>
2026-09-16 21:23:05 -0300

    Create package-info.js for entity predicates
    Add package-info.js for entity advancement predicates

---
+++ net/minecraft/advancements/predicates/entity/package-info.js
@@ -0,0 +1,7 @@
+/**
+ * Pacote: net.minecraft.advancements.predicates.entity
+ * Marcado como NullMarked para sinalizar que os componentes
+ * deste pacote não esperam receber parâmetros nulos por padrão.
+ */
+
+window.AdvancementEntityPredicates = window.AdvancementEntityPredicates || {};
